ff244865a0686a229c0cf3c8fa635ac55d31cf0a
github-thiru <thiruthorlikonda967@gmail.com>
2024-11-15 23:59:32 +0530

    conditions

---
+++ script.js
@@ -0,0 +1,39 @@
+    // first task//
+    var i=10
+    if(i==0){
+        console.log('zero');
+    }
+     else if(i>0){
+        console.log('positive');
+         if(i%2==0){
+            console.log('even');
+        }
+        else{
+            console.log('odd');
+        }
+     }
+     else{
+        console.log('negative');
+     }
+    
+            // second task//
+    var a=4
+    var b=6
+    if(a%2<1&&b%2>=0){
+     console.log(a+b);
+    }
+    else{
+        console.log("not valid");
+    }
+            //  third task//
+     var num=5;
+    for(var i=1;i<=10;i++){
+        console.log(num +" x "+ i +" = " + (num*i));
+    }
+          var num=7
+          var i=1;
+          while(i<=10){
+            console.log(num +" x "+ i +" = " + (num*i));
+            i++;
+          }
+          
